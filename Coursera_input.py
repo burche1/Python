c020b8bb0fdb303cfddba6ec1822cab53f6201cc
burche1 <burche.t.d@gmail.com>
2017-05-30 18:08:05 -0300

    coursera assigments

---
+++ Coursera_input.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# In[11]:
+
+# The code below almost works
+
+name = input("Enter your name ")
+print ("Hello " + name)
+
+
+# In[10]:
+
+hours = input ("Enter hours: ")
+hours = float(hours)
+rate = input ("Enter rate: ")
+rate = float(rate)
+pay = hours * rate
+print (pay)
+
+
+# In[ ]:
+
+
+
